09ffbb682f807a3cdf17a30a84399927959c10c1
abdulsamadsethi <abdulsamadsethi@gmail.com>
2024-10-16 22:51:50 -0400

    adding data folder and jobs

---
+++ data/jobs.js
@@ -0,0 +1,23 @@
+const jobs = [
+    {
+        workplace: "CIBC",
+        position: "Cloud Data Engineer",
+        duration: ["Jan 2022 - Present"],
+        description: [
+            "Utilized Azure Databricks, Azure Data Factory, and ETL tools to monitor, analyze, and manage production servers, scheduled jobs, and incidents",
+            "Supported 30+ production-live applications on a daily basis to ensure on-time data transfer to business stakeholders",
+            "Strategized with Architects and Analysts to resolve issues using analytical tools such as Azure Databricks and Azure Data Factory while collaborating with other IT departments to identify and communicate Root Cause Analysis",
+            "Optimized the data infrastructure of a client-based mobile insight application that displays spending habits of clients with Python, Scala and SQL leading to a 52% productivity increase",
+            "Deployed 50+ data migration applications to production using Jenkins, Git, and Artifactory which streamlined data from Hadoop-based server to Azure Data Lakehouse",
+            "Established 10+ Azure Databricks Notebooks using scripts and commands in Python, PySpark, and SQL generating 3 automated reports with curated data for external clients",
+            "Developed 6 ETL pipelines that transferred automated reports to an ADLS storage container enabling external stakeholders to receive transformed data reports daily",
+            "Created a Python script to remove and monitor duplicate data entries within Azure Data Lake Storage which optimized success rate from 20% to 80% for automated data-ingestion feeds",
+            "Designed a new autonomous Hadoop-based data replication process in Java to transfer data from Production to Disaster Recovery (DR) with an average runtime of 25 minutes per ongoing job",
+            "Worked in a cross-functional team alongside QA, BSAs/BAs, project managers and senior developers to implement the application specification and requirement outlined by clients",
+            "Architected an Oracle Database using SQL logic to store key client encrypted information and error logs which streamlined runtimes from 2-3 days to 2-6 hours for on-demand historical jobs",
+        ],
+        url: "https://cibc.com/",
+    },
+];
+
+export default jobs;
